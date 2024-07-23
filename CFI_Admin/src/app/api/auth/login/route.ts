94450d65c02fefb4fac98c54ecb5580f6902623b
Swadeshit27 <swadeshpal2002@gmail.com>
2024-07-23 20:32:06 +0530

    update

--- CFI_Admin/src/app/api/auth/login/route.ts
+++ CFI_Admin/src/app/api/auth/login/route.ts
@@ -24,6 +24,6 @@ export async function POST(req: NextRequest) {
         return response;
     } catch (error) {
         console.log(error);
-        return NextResponse.json({ message: "Internal Server Error" }, { status: 500 })
+        return NextResponse.json({ message: "Internal Server Error", error }, { status: 500 })
     }
 } 
