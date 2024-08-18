6178d2b283fc47fa78c71b866d217cdc66019bf4
Swadeshit27 <swadeshpal2002@gmail.com>
2024-08-18 17:29:06 +0530

    update

--- CFI_Admin/src/Components/common/LoadingProvider.tsx
+++ CFI_Admin/src/Components/common/LoadingProvider.tsx
@@ -1,11 +1,18 @@
 "use client"
 
-import { useAppSelector } from '@/redux/Store'
-import React from 'react'
+import { useAppDispatch, useAppSelector } from '@/redux/Store'
+import React, { useEffect } from 'react'
 import Loader from './Loader'
+import { setLoading } from '@/redux/slices/AdminSlice'
 
 const Loading = () => {
-    const { loading } = useAppSelector(state => state.admin)
+    const dispatch = useAppDispatch();
+    const { loading } = useAppSelector(state => state.admin);
+    useEffect(() => {
+        setTimeout(() => {
+            dispatch(setLoading(false));
+        }, 3000);
+    }, []);
     return (
         <>
             {
